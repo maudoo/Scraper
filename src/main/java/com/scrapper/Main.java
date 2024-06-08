6dc609be632d2eb8b8423a637c380acb6351995b
Maudoo <144842818+maudoo@users.noreply.github.com>
2024-06-08 15:39:17 -0500

    added functionality to read and write
    added functionality to read and write

--- src/main/java/com/scrapper/Main.java
+++ src/main/java/com/scrapper/Main.java
@@ -1,15 +1,43 @@
 package com.scrapper;
-import com.gargoylesoftware.htmlunit.*;
-import com.gargoylesoftware.htmlunit.html.*;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+import java.util.Scanner;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+ import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        
+        Scanner in = new Scanner(System.in);
         WebClient webClient = createWebClient();
-	    
-		try {
+		HtmlPage currentPage = webClient.getPage("https://www.abdalians.com");	
+
+		// Navigate to a specific page within the website
+		String specificPageUrl = "https://abdalians.com/component/comprofiler/login.html";
+		HtmlPage productPage = webClient.getPage(specificPageUrl);
+
+		HtmlInput usernameField = (HtmlInput) productPage.getElementById("mod_login_username");
+		HtmlInput passwordField = (HtmlInput) productPage.getElementById("mod_login_password");
+
+		System.out.println("enter username");
+		String usernam = in.nextLine();
+		System.out.println("enter pass");
+		String pass = in.nextLine();
+
+		usernameField.setValueAttribute(usernam);
+		passwordField.setValueAttribute(pass);
+
+		// Assuming the submit button has the ID "submitButton"
+		HtmlButton submitButton = (HtmlButton) productPage.getElementByName("submit");
+		HtmlPage loggedInPage = submitButton.click();
+
+		System.out.println("Success!!");
+
+	/*try {
 			String link = "https://www.ebay.com/itm/332852436920?epid=108867251&hash=item4d7f8d1fb8:g:cvYAAOSwOIlb0NGY";
 			HtmlPage page = webClient.getPage(link);
 			
@@ -25,7 +53,8 @@ public class Main {
 			e.printStackTrace();
 		} finally {
 			webClient.close();
-		}	
+		}	 */    
+		
 
     }
 
